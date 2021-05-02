61c11c647d9e62eb708312e5eb13833a96b3a0fa
Erick Cuapio <ecuapio92@gmail.com>
2021-05-01 23:30:45 -0700

    added api

---
+++ assets/script/script.js
@@ -0,0 +1,7 @@
+{
+    "url": "https://cuaer.github.io/Discovering-Erick-Cuapio/",
+      "frameRate": 7,
+      "scrollDown": true,
+      "recordingTimeBeforeAction": 1500,
+      "cookieWindowSelector": ".cookiebar"
+  }
